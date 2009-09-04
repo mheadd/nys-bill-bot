d6d75b64ace46eeb99602fce33e1a86c0a905b9b
Mark Headd <mheadd@voiceingov.org>
2009-09-04 17:12:51 -0400

    Added new class files and license test.

---
+++ classes/class.billformatexception.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Copyright 2009 Mark J. Headd
+ * 
+ * This file is part of IMBills
+ * 
+ * IMBills is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IMBills is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with IMBills.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
+
+/*
+ * Simple class definition to handle malformatted bill IDs.
+ */ 
+class billFormatException extends Exception { }
+
+?>
